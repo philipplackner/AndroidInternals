1070a1742d04d9df90eea72eee81c5256af0e06a
Philipp Lackner <mail@pl-coding.com>
2025-02-19 19:07:01 +0100

    Saved instance state demo

--- app/src/main/java/com/plcoding/androidinternals/MainActivity.kt
+++ app/src/main/java/com/plcoding/androidinternals/MainActivity.kt
@@ -1,32 +1,62 @@
 package com.plcoding.androidinternals
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.plcoding.androidinternals.ui.theme.AndroidInternalsTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var counter by mutableIntStateOf(0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        if(savedInstanceState != null) {
+            counter = savedInstanceState.getInt("counter")
+        }
+
         setContent {
             AndroidInternalsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Button(
+                        onClick = {
+                            counter++
+                        },
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .wrapContentSize()
+                    ) {
+                        Text("Increment: $counter")
+                    }
                 }
             }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("counter", counter)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
     }
 }
 
